f25d2ebc3133a298863e72572a630354e7552b89
Sean Pruss <seanpruss5@gmail.com>
2023-12-23 20:28:50 -0500

    Added hardtime.nvim

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,7 @@
+return {
+    "m4xshen/hardtime.nvim",
+    event = "BufEnter",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+    disabled_filetypes = { "qf", "netrw", "neotree", "lazy", "mason", "oil" },
+}
