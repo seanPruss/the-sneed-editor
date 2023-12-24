3f6fd8c16e8ab60a7065a02431a105c4f93e14a0
Sean Pruss <seanpruss5@gmail.com>
2023-12-24 18:15:29 -0500

    fixed migration to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,4 +1,3 @@
-local harpoon = require("harpoon")
 return {
     "theprimeagen/harpoon",
     branch = "harpoon2",
@@ -8,51 +7,51 @@ return {
         {
             "<leader>a",
             function()
-                harpoon:list():append()
+                require("harpoon"):list():append()
             end,
             desc = "Add to Harpoon list",
         },
         {
             "<leader>h",
             function()
-                harpoon.ui:toggle_quick_menu(harpoon:list())
+                require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
             end,
             desc = "Show Harpoon list",
         },
         {
             "<C-h>",
             function()
-                harpoon:list():select(1)
+                require("harpoon"):list():select(1)
             end,
         },
         {
             "<C-j>",
             function()
-                harpoon:list():select(2)
+                require("harpoon"):list():select(2)
             end,
         },
         {
             "<C-k>",
             function()
-                harpoon:list():select(3)
+                require("harpoon"):list():select(3)
             end,
         },
         {
             "<C-l>",
             function()
-                harpoon:list():select(4)
+                require("harpoon"):list():select(4)
             end,
         },
         {
             "<C-S-P>",
             function()
-                harpoon:list():prev()
+                require("harpoon"):list():prev()
             end,
         },
         {
             "<C-S-N>",
             function()
-                harpoon:list():next()
+                require("harpoon"):list():next()
             end,
         },
     },
