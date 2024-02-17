8265f5dac43f4c57b311884ffc9515351b4a491d
seanPruss <seanpruss5@gmail.com>
2024-02-17 13:45:34 -0500

    harpoon added to disabled file types for hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -3,5 +3,5 @@ return {
     event = "BufReadPre",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {},
-    disabled_filetypes = { "qf", "netrw", "neotree", "lazy", "mason", "oil" },
+    disabled_filetypes = { "qf", "netrw", "neo-tree", "lazy", "mason", "oil", "harpoon-menu" },
 }
