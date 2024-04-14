3342c689156a3e2e4df88ad88fe236960e291a8c
seanPruss <seanpruss5@gmail.com>
2024-04-14 16:54:31 -0400

    change a couple keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,9 +13,8 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word" })
 vim.keymap.set("n", "<leader>m", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make executable" })
-vim.keymap.set("n", "<leader>;", "A;", { desc = "Add semicolon" })
 vim.keymap.set("n", "<leader>[", "A {<CR><CR>}<Esc>kcc", { desc = "Create squirly braces" })
-vim.keymap.set("n", "<leader>i", "A:<Esc>o", { desc = "Add colon + new line" })
+vim.keymap.set("n", "<leader>;", "A:<Esc>o", { desc = "Add colon + new line" })
 vim.keymap.set("n", "j", "jzz")
 vim.keymap.set("n", "k", "kzz")
 vim.keymap.set("n", "<CR>", "<CR>zz")
@@ -23,7 +22,7 @@ vim.keymap.set("n", "-", "-zz")
 vim.keymap.set("n", "G", "Gzz")
 vim.keymap.set(
     "n",
-    "<leader>k",
+    "<leader>i",
     [[i<!DOCTYPE html><CR><html lang="en"><CR><head><CR><meta charset="UTF-8"><CR><meta http-equiv="X-UA-Compatible" content="IE=edge"><CR><meta name="viewport" content="width=device-width, initial-scale=1"><CR><title>Document</title><CR></head><CR><body><CR><CR></body><CR></html><Esc>kkcc]]
 )
 vim.keymap.set("n", "<C-s>", "<nop>")
